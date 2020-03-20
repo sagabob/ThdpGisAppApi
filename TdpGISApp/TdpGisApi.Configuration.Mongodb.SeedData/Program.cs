d6a8daeedd247b86c1d052b9452f811bb132fa36
Bob Pham <bobpham.tdp@live.com>
2020-03-21 00:19:34 +1100

    Add SeedData project

---
+++ TdpGISApp/TdpGisApi.Configuration.Mongodb.SeedData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TdpGisApi.Configuration.Mongodb.SeedData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
