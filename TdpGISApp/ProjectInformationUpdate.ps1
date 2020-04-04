8679b3334b29f1a582292a70fa27127a52e4d6fb
Bob Pham <bobpham.tdp@live.com>
2020-04-04 22:39:45 +1100

    work in progress

--- TdpGISApp/ProjectInformationUpdate.ps1
+++ TdpGISApp/ProjectInformationUpdate.ps1
@@ -8,17 +8,14 @@ $TagHolder = "Unknown-BuiltTag"
 
 $KeyHolder = "--key--"
 
-$KeyValue = $ConnectionKey
+$KeyValue = "$(ConnectionKey)"
 
-Write-Output "Number of files: $files.Length"
 
 foreach ($file in $files) {
     
     $filecontent = Get-Content($file)
    
-    $filecontent -replace $TagHolder, $Env:BUILD_BUILDNUMBER | Out-File $file
-
-    $filecontent -replace $KeyHolder, $KeyValue | Out-File $file
+    $filecontent -replace $TagHolder, $Env:BUILD_BUILDNUMBER | Out-File $file     
 
     Write-Output "$file.FullName - Built Tag is updated"
 }
@@ -29,7 +26,7 @@ foreach ($file in $files) {
      
     $filecontent -replace $KeyHolder, $KeyValue | Out-File $file
 
-    Write-Output "$file.FullName - Update the key $(KeyValue)"
+    Write-Output "$file.FullName - Update the key $KeyValue"
 }
 
 
